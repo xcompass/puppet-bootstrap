cb3bdca5361adbfcabace4f31830efcb10a1d104
Compass <pan.luo@ubc.ca>
2013-04-22 21:02:08 -0700

    Add Puppetfile and bootstrap librarian-puppet

---
+++ Puppetfile
@@ -0,0 +1,13 @@
+forge "http://forge.puppetlabs.com"
+
+mod 'puppetlabs/stdlib'
+mod 'spiette/selinux'
+
+mod 'epel',
+    :git => 'https://github.com/stahnma/puppet-module-epel.git'
+
+mod 'nginx',
+    :git => 'https://github.com/jfryman/puppet-nginx.git'
+
+mod 'php',
+    :git => 'https://github.com/thias/puppet-php.git'
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,17 @@
-!/bin/bash
+#!/bin/bash
 
 rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm
-yum install puppet
+yum -y install puppet
 
 gem install librarian-puppet
+
+puppet resource package git ensure=present
+
+# clone the bootstrap repo 
+git clone https://github.com/xcompass/puppet-bootstrap.git puppet
+cd puppet
+
+# install the modules
+librarian-puppet install
+
+puppet apply --modulepath=modules base.pp
